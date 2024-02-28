2627bc58051edac560457dfc087e97fc2b16146b
Jordan Miles <j@jmiles42.com>
2024-02-29 01:31:07 +1100

    Changed around the Operand code

--- Jellyfin.Plugin.SmartPlaylist/QueryEngine/Operand.cs
+++ Jellyfin.Plugin.SmartPlaylist/QueryEngine/Operand.cs
@@ -1,90 +1,413 @@
 ﻿using System.Text;
+using Jellyfin.Data.Entities;
 using Jellyfin.Plugin.SmartPlaylist.Infrastructure;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
 
 namespace Jellyfin.Plugin.SmartPlaylist.QueryEngine;
 
 #pragma warning disable CA1822 // Mark members as static
-public class Operand {
+public class Operand
+{
+	public BaseItem BaseItem { get; }
 
-	public List<string> Actors                   { get; set; } = new();
-	public List<string> Artists                  { get; set; } = new();
-	public List<string> Composers                { get; set; } = new();
-	public List<string> Directors                { get; set; } = new();
-	public List<string> Genres                   { get; set; } = new();
-	public List<string> GuestStars               { get; set; } = new();
-	public List<string> Producers                { get; set; } = new();
-	public List<string> Studios                  { get; set; } = new();
-	public List<string> Writers                  { get; set; } = new();
-	public List<string> Tags                     { get; set; } = new();
-	public bool         Exists                   => true;
-	public bool         HasSubtitles             { get; set; } = false;
-	public bool         IsFavoriteOrLiked        { get; set; } = false;
-	public bool         IsHorizontal             => Height < Width;
-	public bool         IsPlayed                 { get; set; } = false;
-	public bool         IsSquare                 => Height == Width;
-	public bool         IsVertical               => !IsHorizontal;
-	public double       DateCreated              { get; set; } = 0;
-	public double       DateLastRefreshed        { get; set; } = 0;
-	public double       DateLastSaved            { get; set; } = 0;
-	public double       DateModified             { get; set; } = 0;
-	public double       PlayedPercentage         { get; set; } = 0;
-	public double       PremiereDate             { get; set; } = 0;
-	public double?      LastPlayedDate           { get; set; } = null;
-	public float        CommunityRating          { get; set; } = 0;
-	public float        CriticRating             { get; set; } = 0;
-	public int          DaysSinceCreated         { get; set; } = 0;
-	public int          DaysSinceLastModified    { get; set; } = 0;
-	public int          DaysSinceLastRefreshed   { get; set; } = 0;
-	public int          DaysSinceLastSaved       { get; set; } = 0;
-	public int          DaysSincePremiereDate    { get; set; } = 0;
-	public int          Height                   { get; set; } = 0;
-	public int          PlayedCount              { get; set; } = 0;
-	public int          Width                    { get; set; } = 0;
-	public int?         AiredSeasonNumber        { get; set; } = null;
-	public int?         ParentIndexNumber        { get; set; } = null;
-	public int?         ProductionYear           { get; set; } = null;
-	public long         PlaybackPositionTicks    { get; set; } = 0;
-	public string       Album                    { get; set; } = string.Empty;
-	public string       CollectionName           { get; set; } = string.Empty;
-	public string       Container                { get; set; } = string.Empty;
-	public string       FileNameWithoutExtension { get; set; } = string.Empty;
-	public string       FolderPath               { get; set; } = string.Empty;
-	public string       GrandparentName          { get; set; } = string.Empty;
-	public string       MediaType                { get; set; } = string.Empty;
-	public string       Name                     { get; set; }
-	public string       OfficialRating           { get; set; } = string.Empty;
-	public string       OriginalTitle            { get; set; } = string.Empty;
-	public string       Overview                 { get; set; } = string.Empty;
-	public string       ParentName               { get; set; } = string.Empty;
-	public string       SeasonName               { get; set; } = string.Empty;
-	public string       SeriesName               { get; set; } = string.Empty;
-	public string       SortName                 { get; set; } = string.Empty;
-	public TimeSpan     Length                   => TimeSpan.FromTicks(LengthTicks);
-	public double       LengthSeconds            => Length.TotalSeconds;
-	public double       LengthMinutes            => Length.TotalMinutes;
-	public double       LengthHours              => Length.TotalHours;
-	public long         LengthTicks              { get; set; } = 0;
+	private readonly IUserDataManager _userDataManager;
+	private readonly User             _user;
+	private readonly ILibraryManager  _libraryManager;
+
+	public Operand(ILibraryManager libraryManager, BaseItem baseItem, IUserDataManager userDataManager, User user) {
+		BaseItem         = baseItem;
+		_userDataManager = userDataManager;
+		_user            = user;
+		_libraryManager  = libraryManager;
+		Name             = baseItem.Name;
+
+		CommunityRating          = baseItem.CommunityRating.GetValueOrDefault();
+		CriticRating             = baseItem.CriticRating.GetValueOrDefault();
+		MediaType                = baseItem.MediaType;
+		Album                    = baseItem.Album;
+		FolderPath               = baseItem.ContainingFolderPath;
+		ProductionYear           = baseItem.ProductionYear;
+		OriginalTitle            = baseItem.OriginalTitle;
+		Height                   = baseItem.Height;
+		Width                    = baseItem.Width;
+		FileNameWithoutExtension = baseItem.FileNameWithoutExtension;
+		OfficialRating           = baseItem.OfficialRating;
+		SortName                 = baseItem.SortName;
+		DaysSinceCreated         = GetDaysAgo(baseItem.DateCreated);
+		DaysSinceLastRefreshed   = GetDaysAgo(baseItem.DateLastRefreshed);
+		DaysSinceLastSaved       = GetDaysAgo(baseItem.DateLastSaved);
+		DaysSinceLastModified    = GetDaysAgo(baseItem.DateModified);
+		Overview                 = baseItem.Overview;
+		Container                = baseItem.Container;
+		ParentName               = baseItem.LatestItemsIndexContainer?.Name;
+		GrandparentName          = baseItem.LatestItemsIndexContainer?.DisplayParent?.Name;
+
+		if (baseItem.PremiereDate.HasValue) {
+			DaysSincePremiereDate = GetDaysAgo(baseItem.PremiereDate.Value, DateTime.Now);
+			PremiereDate          = GetUnixSeconds(baseItem.PremiereDate.Value);
+		}
+
+		DateCreated       = GetUnixSeconds(baseItem.DateCreated);
+		DateLastRefreshed = GetUnixSeconds(baseItem.DateLastRefreshed);
+		DateLastSaved     = GetUnixSeconds(baseItem.DateLastSaved);
+		DateModified      = GetUnixSeconds(baseItem.DateModified);
+
+		switch (baseItem) {
+			case MediaBrowser.Controller.Entities.Movies.Movie movie:
+				HasSubtitles   = movie.HasSubtitles;
+				CollectionName = movie.CollectionName;
+				LengthTicks    = movie.RunTimeTicks ?? 0;
+
+				break;
+			case MediaBrowser.Controller.Entities.TV.Episode episode:
+				HasSubtitles      = episode.HasSubtitles;
+				AiredSeasonNumber = episode.AiredSeasonNumber;
+				ParentIndexNumber = episode.ParentIndexNumber;
+				SeasonName        = episode.SeasonName;
+				SeriesName        = episode.SeriesName;
+				LengthTicks       = episode.RunTimeTicks ?? 0;
+
+				break;
+			case MusicVideo musicVideo:
+				HasSubtitles = musicVideo.HasSubtitles;
+				LengthTicks  = musicVideo.RunTimeTicks ?? 0;
+
+				break;
+		}
+
+		var data = userDataManager.GetUserData(user, baseItem);
+
+		if (data is not null) {
+			IsPlayed    = data.Played;
+			PlayedCount = data.PlayCount;
+
+			if (data.LastPlayedDate.HasValue) {
+				LastPlayedDate = GetUnixSeconds(data.LastPlayedDate.Value);
+			}
+
+			IsFavoriteOrLiked     = data.IsFavorite;
+			PlaybackPositionTicks = data.PlaybackPositionTicks;
+
+
+
+			if (!baseItem.RunTimeTicks.HasValue) {
+				return;
+			}
+
+			double pct = baseItem.RunTimeTicks.Value;
+
+			if (pct <= 0) {
+				return;
+			}
+
+			pct = data.PlaybackPositionTicks / pct;
+
+			if (pct > 0) {
+				PlayedPercentage = 100 * pct;
+			}
+		}
+	}
+
+	private static double GetUnixSeconds(DateTime datetime) {
+		try {
+			return new DateTimeOffset(datetime).ToUnixTimeSeconds();
+		}
+		catch {
+			//Ignore
+		}
+
+		return 0;
+	}
+
+	public static int GetDaysAgo(DateTime currentDate) => GetDaysAgo(currentDate, DateTime.Now);
+
+	public static int GetDaysAgo(DateTime currentDate, DateTime now) => (int)(now - currentDate).TotalDays;
+
+	public List<string> Actors {
+		get {
+			if (_actors is not null) {
+				return _actors;
+			}
+
+			var people = GetPeople();
+
+			_actors = new(people.Where(x => x.Type.Equals("Actor", StringComparison.OrdinalIgnoreCase))
+								.Select(x => x.Name));
+
+			return _actors;
+		}
+	}
+
+	public List<string> Artists {
+		get {
+			if (_artists is not null) {
+				return _artists;
+			}
+
+			_artists = new();
+
+
+			if (BaseItem is MusicVideo mv) {
+				_artists.AddRange(mv.Artists);
+			}
+
+			return _artists;
+		}
+	}
+
+	public List<string> Composers {
+		get {
+			if (_composers is not null) {
+				return _composers;
+			}
+
+			var people = GetPeople();
+
+			_composers = new(people.Where(x => x.Type.Equals("Composer", StringComparison.OrdinalIgnoreCase))
+								   .Select(x => x.Name));
+
+			return _composers;
+		}
+	}
+
+	public List<string> Directors {
+		get {
+			if (_directors is not null) {
+				return _directors;
+			}
+
+			var people = GetPeople();
+
+			_directors = new(people.Where(x => x.Type.Equals("Director", StringComparison.OrdinalIgnoreCase))
+								   .Select(x => x.Name));
+
+			return _directors;
+		}
+	}
+
+	public List<string> GuestStars {
+		get {
+			if (_guestStars is not null) {
+				return _guestStars;
+			}
+
+			var people = GetPeople();
+
+			_guestStars = new(people.Where(x => x.Type.Equals("GuestStar", StringComparison.OrdinalIgnoreCase))
+									.Select(x => x.Name));
+
+			return _guestStars;
+		}
+	}
+
+	public List<string> Producers {
+		get {
+			if (_producers is not null) {
+				return _producers;
+			}
+
+			var people = GetPeople();
+
+			_producers = new(people.Where(x => x.Type.Equals("Producer", StringComparison.OrdinalIgnoreCase))
+								   .Select(x => x.Name));
+
+			return _producers;
+		}
+	}
+
+	public List<string> Writers {
+		get {
+			if (_writers is not null) {
+				return _writers;
+			}
+
+			var people = GetPeople();
+
+			_writers = new(people.Where(x => x.Type.Equals("Writer", StringComparison.OrdinalIgnoreCase))
+								 .Select(x => x.Name));
+
+			return _writers;
+		}
+	}
+
+	public List<string> Genres {
+		get {
+			if (_genres is not null) {
+				return _genres;
+			}
+
+			_genres = new();
+			_genres.AddRange(BaseItem.Genres);
+
+			return _genres;
+		}
+	}
+
+	public List<string> Studios {
+		get {
+			if (_studios is not null) {
+				return _studios;
+			}
+
+			_studios = new();
+			_studios.AddRange(BaseItem.Studios);
+
+			return _studios;
+		}
+	}
+
+	public List<string> Tags {
+		get {
+			if (_tags is not null) {
+				return _tags;
+			}
+
+			_tags = new();
+			_tags.AddRange(BaseItem.Tags);
+
+			return _tags;
+		}
+	}
+
+	private List<string>? _actors;
+	private List<string>? _artists;
+	private List<string>? _composers;
+	private List<string>? _directors;
+	private List<string>? _guestStars;
+	private List<string>? _producers;
+	private List<string>? _writers;
+	private List<string>? _genres;
+	private List<string>? _studios;
+	private List<string>? _tags;
+
+	private List<PersonInfo>? _people;
+
+	private List<PersonInfo> GetPeople() {
+		if (_people is not null) {
+			return _people;
+		}
+
+		return _people = _libraryManager.GetPeople(BaseItem);
+	}
+
+	public bool Exists => true;
+
+	public bool HasSubtitles { get; }
+
+	public bool IsFavoriteOrLiked { get; }
+
+	public bool IsHorizontal => Height < Width;
+
+	public bool IsPlayed { get; }
+
+	public bool IsSquare => Height == Width;
+
+	public bool IsVertical => !IsHorizontal;
+
+	public double DateCreated { get; }
+
+	public double DateLastRefreshed { get; }
+
+	public double DateLastSaved { get; }
+
+	public double DateModified { get; }
+
+	public double PlayedPercentage { get; }
+
+	public double PremiereDate { get; }
+
+	public double? LastPlayedDate { get; }
+
+	public float CommunityRating { get; }
+
+	public float CriticRating { get; }
+
+	public int DaysSinceCreated { get; }
+
+	public int DaysSinceLastModified { get; }
+
+	public int DaysSinceLastRefreshed { get; }
+
+	public int DaysSinceLastSaved { get; }
+
+	public int DaysSincePremiereDate { get; }
+
+	public int Height { get; }
+
+	public int PlayedCount { get; }
+
+	public int Width { get; }
+
+	public int? AiredSeasonNumber { get; }
+
+	public int? ParentIndexNumber { get; }
+
+	public int? ProductionYear { get; }
+
+	public long PlaybackPositionTicks { get; }
+
+	public string? Album { get; }
+
+	public string? CollectionName { get; }
+
+	public string? Container { get; }
+
+	public string? FileNameWithoutExtension { get; }
+
+	public string? FolderPath { get; }
+
+	public string? GrandparentName { get; }
+
+	public string? MediaType { get; }
+
+	public string? Name { get; }
+
+	public string? OfficialRating { get; }
+
+	public string? OriginalTitle { get; }
+
+	public string? Overview { get; }
+
+	public string? ParentName { get; }
+
+	public string? SeasonName { get; }
+
+	public string? SeriesName { get; }
+
+	public string? SortName { get; }
+
+	public TimeSpan Length => TimeSpan.FromTicks(LengthTicks);
+
+	public double LengthSeconds => Length.TotalSeconds;
+
+	public double LengthMinutes => Length.TotalMinutes;
+
+	public double LengthHours => Length.TotalHours;
+
+	public long LengthTicks { get; }
 
 	public string AllText {
 		get {
 			if (string.IsNullOrEmpty(_allText)) {
 				_allText = GetAllText();
 			}
+
 			return _allText;
 		}
 	}
 
-	private string _allText;
-
-	public Operand(string name) {
-		Name = name;
-	}
+	private string? _allText;
 
 	private string GetAllText() {
 		var sb = new StringBuilder();
 
 		Actors.ForEach(a => sb.AppendLineIfNotNullOrEmpty(a));
-		Artists.ForEach(a => sb.AppendLineIfNotNullOrEmpty(a));
+
+		foreach (var a in Artists) {
+			sb.AppendLineIfNotNullOrEmpty(a);
+		}
+
 		Composers.ForEach(a => sb.AppendLineIfNotNullOrEmpty(a));
 		Directors.ForEach(a => sb.AppendLineIfNotNullOrEmpty(a));
 		GuestStars.ForEach(a => sb.AppendLineIfNotNullOrEmpty(a));
@@ -106,6 +429,7 @@ public class Operand {
 		sb.AppendLineIfNotNullOrEmpty(SeasonName);
 		sb.AppendLineIfNotNullOrEmpty(SeriesName);
 		sb.AppendLineIfNotNullOrEmpty(SortName);
+
 		return sb.ToString();
 	}
 }
